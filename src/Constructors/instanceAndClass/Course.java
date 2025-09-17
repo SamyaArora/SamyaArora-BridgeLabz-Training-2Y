575e90f9f1443313d5d1dc127b10b5eab90c7847
SamyaArora <samya.arora_cs.aiml24@gla.ac.in>
2025-09-17 15:43:15 +0530

    Oops done

---
+++ src/Constructors/instanceAndClass/Course.java
@@ -0,0 +1,25 @@
+package Constructors.instanceAndClass;
+
+public class Course {private String courseName;
+    private int duration;
+    private double fee;
+    private static String instituteName;
+
+    public Course(String courseName, int duration, double fee) {
+        this.courseName = courseName;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    public void displayCourseDetails() {
+        System.out.println("Institute Name: " + instituteName);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+        System.out.println("Fee: ₹" + fee);
+    }
+
+    public static void updateInstituteName(String newInstituteName) {
+        instituteName = newInstituteName;
+    }
+
+}
---
+++ src/Constructors/level1/Book2.java
@@ -0,0 +1,44 @@
+package Constructors.level1;
+
+public class Book2 {  private String title;
+    private String author;
+    private double price;
+    private boolean availability;
+
+    public Book2(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.availability = true;
+    }
+
+    public boolean borrowBook() {
+        if (availability) {
+            availability = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.printf("Price: $%.2f%n", price);
+        System.out.println("Availability: " + (availability ? "Available" : "Not Available"));
+    }
+
+    public static void main(String[] args) {
+        Book2 book1 = new Book2("1984", "George Orwell", 12.99);
+        System.out.println("Initial Book Details:");
+        book1.displayDetails();
+
+        if (book1.borrowBook()) {
+            System.out.println("\nBook borrowed successfully.");
+        } else {
+            System.out.println("\nBook is not available for borrowing.");
+        }
+        System.out.println("Updated Book Details:");
+        book1.displayDetails();
+    }
+
+}
