fd86908739ea2480837f3f54515813dfc3eb5170
SamyaArora <samya.arora_cs.aiml24@gla.ac.in>
2025-09-18 15:51:54 +0530

    Add source code for Java Classes and Objects modules

---
+++ src/Oops/Java_Classes_and_Objects/Level1/MobilePhone.java
@@ -0,0 +1,25 @@
+package Oops.Java_Classes_and_Objects.Level1;
+
+public class MobilePhone { private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Samsung", "Galaxy S23", 79999.0);
+        phone.displayDetails();
+    }
+}
+
+
---
+++ src/Oops/Java_Classes_and_Objects/Level2/PalindromeChecker.java
@@ -0,0 +1,26 @@
+package Oops.Java_Classes_and_Objects.Level2;
+
+public class PalindromeChecker { private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+
+    public void displayResult() {
+        System.out.println("Text: " + text);
+        System.out.println("Is palindrome: " + isPalindrome());
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("A man a plan a canal Panama");
+        checker.displayResult();
+    }
+}
+
+
