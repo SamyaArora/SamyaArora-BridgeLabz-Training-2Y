83047f7d3cf2fa2d977c38444b2f5181f2e4be0e
SamyaArora <samya.arora_cs.aiml24@gla.ac.in>
2025-11-06 16:27:30 +0530

    commit assignment of Wrapper Class completed

---
+++ WrapperClass/EmployeeDataProcessing.java
@@ -0,0 +1,16 @@
+package WrapperClass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class EmployeeDataProcessing {
+    public static void main(String[] args){
+        int[] ages = {34, 28, 45, 22, 39};
+        ArrayList<Integer> ageList = new ArrayList<>();
+        for(int a : ages) ageList.add(a);
+        int youngest = Collections.min(ageList);
+        int oldest = Collections.max(ageList);
+        System.out.println("Youngest age = " + youngest);
+        System.out.println("Oldest age = " + oldest);
+    }
+}
